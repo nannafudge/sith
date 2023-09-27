e6c79c33274a63b1789383ce4f30029a7a10a755
nannafudge <6781691+nannafudge@users.noreply.github.com>
2023-09-27 14:04:37 +0100

    [#1] - Implemented unit tests for sith::core functions.

--- src/common.rs
+++ src/common.rs
@@ -9,9 +9,14 @@ use syn::{
     }
 };
 
+const DELIM_DEBUG: [&str; 4] = ["()", "{}", "[]", "INVIS"];
+
 pub fn attribute_name_to_string(attr: &Attribute) -> String {
-    let segments = attr.meta.path().segments.iter().rev();
-    segments.last().map_or(String::default(), | segment | segment.ident.to_string())
+    attr.meta.path().segments.iter()
+        .next_back()
+        .map_or(String::default(), | segment |
+            segment.ident.to_string()
+        )
 }
 
 pub fn parse_group_with_delim(delim: Delimiter, input: ParseStream) -> Result<TokenStream> {
@@ -20,7 +25,8 @@ pub fn parse_group_with_delim(delim: Delimiter, input: ParseStream) -> Result<To
             return Ok((content.token_stream(), next));
         }
 
-        Err(cursor.error(format!("expected delimiter: {:?}", &delim)))
+        // isize can be cast as usize in this case - there's no negative values
+        Err(cursor.error(format!("expected '{}'", DELIM_DEBUG[delim as usize])))
     })
 }
 
@@ -55,6 +61,9 @@ pub fn parse_next_tt(input: ParseStream) -> Result<TokenTree> {
 #[macro_use]
 pub(crate) mod macros {
     macro_rules! error_spanned {
+        ($error:literal) => {
+            syn::Error::new(proc_macro2::Span::call_site(), $error)
+        };
         ($error:literal, $item:expr) => {
             syn::Error::new(syn::spanned::Spanned::span($item), $error)
         };
@@ -108,6 +117,66 @@ pub(crate) mod tests {
         };
 
         #[test]
+        fn ident() {
+            let attr = construct_attribute!(
+                AttrStyle::Outer,
+                construct_attribute_meta!(test)
+            );
+
+            assert_eq!(attribute_name_to_string(&attr).as_str(), "test");
+        }
+
+        #[test]
+        fn ident_path() {
+            let attr = construct_attribute!(
+                AttrStyle::Outer,
+                construct_attribute_meta!(my::path::to::test)
+            );
+
+            assert_eq!(attribute_name_to_string(&attr).as_str(), "test");
+        }
+
+        #[test]
+        fn list() {
+            let attr = construct_attribute!(
+                AttrStyle::Outer,
+                construct_attribute_meta!(test(one, two))
+            );
+
+            assert_eq!(attribute_name_to_string(&attr).as_str(), "test");
+        }
+
+        #[test]
+        fn list_path() {
+            let attr = construct_attribute!(
+                AttrStyle::Outer,
+                construct_attribute_meta!(path::to::my::test(one, two))
+            );
+
+            assert_eq!(attribute_name_to_string(&attr).as_str(), "test");
+        }
+
+        #[test]
+        fn name_value() {
+            let attr = construct_attribute!(
+                AttrStyle::Outer,
+                construct_attribute_meta!(test = 123)
+            );
+
+            assert_eq!(attribute_name_to_string(&attr).as_str(), "test");
+        }
+
+        #[test]
+        fn name_value_path() {
+            let attr = construct_attribute!(
+                AttrStyle::Outer,
+                construct_attribute_meta!(path::to::my::test = 123)
+            );
+
+            assert_eq!(attribute_name_to_string(&attr).as_str(), "test");
+        }
+
+        #[test]
         fn empty() {
             let attr = construct_attribute!(
                 AttrStyle::Outer,
@@ -116,18 +185,186 @@ pub(crate) mod tests {
 
             assert_eq!(attribute_name_to_string(&attr).as_str(), "");
         }
+    }
+
+    mod greedy_parse_with_delim {
+        use super::*;
+        
+        use core::marker::PhantomData;
+        use syn::Token;
+        use proc_macro2::{
+            Literal, Group
+        };
+
+        struct VecLiteralShim<D: Parse>(Vec<Literal>, PhantomData<D>);
+
+        impl<D: Parse> core::fmt::Debug for VecLiteralShim<D> {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                f.debug_tuple("VecLiteralShim").field(&self.0).finish()
+            }
+        }
+
+        impl<D: Parse> ToTokens for VecLiteralShim<D> {
+            fn to_tokens(&self, tokens: &mut TokenStream) {
+                self.0.iter().for_each(| item | item.to_tokens(tokens));
+            }
+        }
+
+        impl<D: Parse> Parse for VecLiteralShim<D> {
+            fn parse(input: ParseStream) -> Result<Self> {
+                let out = greedy_parse_with_delim::<Literal, D>(input);
+                assert!(input.is_empty(), "greedy_parse: failed to capture all tokens");
+
+                Ok(Self(out?, PhantomData))
+            }
+        }
+
+        impl<D: Parse> VecLiteralShim<D> {
+            fn new(inner: Vec<Literal>) -> Self {
+                Self(inner, PhantomData)
+            }
+        }
+
+        type CommaSeperated = VecLiteralShim::<Token![,]>;
+        type GroupSeperated = VecLiteralShim::<Group>;
 
         #[test]
-        fn path() {
-            let attr = construct_attribute!(
-                AttrStyle::Outer,
-                construct_attribute_meta!(test)
-            );
+        fn single() {
+            impl_parse_shim!(CommaSeperated, CommaSeperated::parse);
 
-            assert_eq!(attribute_name_to_string(&attr).as_str(), "test");
+            assert_eq_with_shim!(
+                syn::parse2::<ParseShim<CommaSeperated>>(quote!("foo")),
+                Ok(CommaSeperated::new(Vec::from([Literal::string("foo")])))
+            );
+        }
+
+        #[test]
+        fn many() {
+            impl_parse_shim!(CommaSeperated, CommaSeperated::parse);
+
+            assert_eq_with_shim!(
+                syn::parse2::<ParseShim<CommaSeperated>>(quote!("foo", "bar")),
+                Ok(CommaSeperated::new(Vec::from([Literal::string("foo"), Literal::string("bar")])))
+            );
+        }
+
+        #[test]
+        fn arbitrary_delims() {
+            impl_parse_shim!(GroupSeperated, GroupSeperated::parse);
+
+            assert_eq_with_shim!(
+                syn::parse2::<ParseShim<GroupSeperated>>(quote!("foo" (123, 456) "bar" (789))),
+                Ok(GroupSeperated::new(Vec::from([Literal::string("foo"), Literal::string("bar")])))
+            );
+        }
+
+        #[test]
+        fn invalid_delim() {
+            fn parse_delim_comma_shim(input: ParseStream) -> Result<Vec<Literal>> {
+                greedy_parse_with_delim::<Literal, Token![,]>(input)
+            }
+
+            impl_parse_shim!(Vec<Literal>, parse_delim_comma_shim);
+
+            // Error message generation is delegated to syn's implementation here,
+            // so we shouldn't explicity test against such in case it changes in the future
+            assert!(syn::parse2::<ParseShim<Vec<Literal>>>(quote!("foo";)).is_err());
+        }
+
+        #[test]
+        fn empty() {
+            impl_parse_shim!(CommaSeperated, CommaSeperated::parse);
+            assert_eq_with_shim!(
+                syn::parse2::<ParseShim<CommaSeperated>>(quote!()),
+                Ok(CommaSeperated::new(Vec::new()))
+            );
         }
     }
 
+    mod parse_group_with_delim {
+        use super::*;
+
+        use proc_macro2::Delimiter;
+
+        fn parse_parenthesis_shim(input: ParseStream) -> Result<TokenStream> {
+            parse_group_with_delim(Delimiter::Parenthesis, input)
+        }
+
+        fn parse_braces_shim(input: ParseStream) -> Result<TokenStream> {
+            parse_group_with_delim(Delimiter::Brace, input)
+        }
+
+        fn parse_bracket_shim(input: ParseStream) -> Result<TokenStream> {
+            parse_group_with_delim(Delimiter::Bracket, input)
+        }
+
+        #[test]
+        fn parenthesis() {
+            impl_parse_shim!(TokenStream, parse_parenthesis_shim);
+
+            assert_eq_with_shim!(
+                syn::parse2::<ParseShim<TokenStream>>(quote!((test, 123))),
+                Ok(quote!(test, 123))
+            );
+        }
+
+        #[test]
+        fn parenthesis_invalid() {
+            impl_parse_shim!(TokenStream, parse_parenthesis_shim);
+
+            assert_eq_with_shim!(
+                syn::parse2::<ParseShim<TokenStream>>(quote!([test, 123])),
+                Err(error_spanned!("expected '()'"))
+            );
+        }
+
+        #[test]
+        fn braces() {
+            impl_parse_shim!(TokenStream, parse_braces_shim);
+
+            assert_eq_with_shim!(
+                syn::parse2::<ParseShim<TokenStream>>(quote!({test, 123})),
+                Ok(quote!(test, 123))
+            );
+        }
+
+        #[test]
+        fn braces_invalid() {
+            impl_parse_shim!(TokenStream, parse_braces_shim);
+
+            assert_eq_with_shim!(
+                syn::parse2::<ParseShim<TokenStream>>(quote!([test, 123])),
+                Err(error_spanned!("expected '{}'"))
+            );
+        }
+
+        #[test]
+        fn bracket() {
+            impl_parse_shim!(TokenStream, parse_bracket_shim);
+
+            assert_eq_with_shim!(
+                syn::parse2::<ParseShim<TokenStream>>(quote!([test, 123])),
+                Ok(quote!(test, 123))
+            );
+        }
+
+        #[test]
+        fn bracket_invalid() {
+            impl_parse_shim!(TokenStream, parse_bracket_shim);
+
+            assert_eq_with_shim!(
+                syn::parse2::<ParseShim<TokenStream>>(quote!({test, 123})),
+                Err(error_spanned!("expected '[]'"))
+            );
+        }
+
+        #[test]
+        fn none() {
+            impl_parse_shim!(TokenStream, parse_parenthesis_shim);
+
+            assert!(syn::parse2::<ParseShim<TokenStream>>(quote!()).is_err());
+        }
+    }
 
     mod parse_next_tt {
         use super::*;
