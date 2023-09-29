6bf82f687fc254dc250369a32c23cec677700fea
nannafudge <6781691+nannafudge@users.noreply.github.com>
2023-09-29 01:41:30 +0100

    [#1] - Added unit tests for `#[test_suite]`. Some testing will be delegated to integration testing.

--- src/core/test_suite/mod.rs
+++ src/core/test_suite/mod.rs
@@ -1,4 +1,3 @@
-use core::mem::take;
 use proc_macro2::TokenStream;
 
 use quote::{
@@ -15,6 +14,12 @@ use syn::{
         Mod, Brace
     }
 };
+use core::{
+    fmt::{
+        Debug, Formatter
+    },
+    mem::take
+};
 use crate::{
     core::{
         Mutate, Mutators,
@@ -23,11 +28,11 @@ use crate::{
     params::{
         setup::*, teardown::*
     },
-    common::attribute_name_to_string
+    common::{attribute_name_to_string, macros::error_spanned}
 };
 
 #[repr(u8)]
-#[derive(Clone, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord)]
 enum SuiteMutator {
     // Mutators should be defined in the order they must apply
     Setup(ParamSetup),
@@ -107,7 +112,10 @@ impl Mutate for TestSuite {
 
 impl Parse for TestSuite {
     fn parse(input: ParseStream) -> Result<Self> {
-        let mut target: ItemMod = input.parse::<ItemMod>()?;
+        let Result::Ok(mut target) = input.parse::<ItemMod>() else {
+            return Err(error_spanned!("#[test_suite] can only be applied to modules", &input.span()));
+        };
+
         let Some(mut contents) = take(&mut target.content) else {
             return Ok( Self { name: target.ident, mutators: None, contents: None } );
         };
@@ -149,6 +157,21 @@ impl ToTokens for TestSuite {
                 contents.iter().for_each(| item | item.to_tokens(suite_inner));
             }
         });
+    }
+}
+
+impl Debug for TestSuite {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("TestSuite")
+            .field("name", &self.name)
+            .field("mutators", &self.mutators)
+            .field("contents", &self.contents.as_ref().map(| items | {
+                items.iter().fold(TokenStream::new(), | mut acc, item | {
+                    item.to_tokens(&mut acc);
+                    acc
+                })
+            }))
+            .finish()
     }
 }
 
@@ -195,4 +218,327 @@ fn is_test_attribute(attributes: &[Attribute]) -> bool {
             name.as_str() == TestCase::RUSTC_TEST_IDENT ||
             name.as_str() == TestCase::WASM_TEST_IDENT
         })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::common::{tests::macros::*, macros::error_spanned};
+
+    use quote::quote;
+    use syn::parse_quote;
+
+    #[test]
+    fn parse_consumes_setup_function() {
+        assert_eq_parsed!(
+            syn::parse2::<TestSuite>(quote!{
+                mod my_suite {
+                    #[setup]
+                    fn setup() {}
+
+                    #[test_case]
+                    fn foo() {}
+                }
+            }),
+            Ok(TestSuite {
+                name: parse_quote!(my_suite),
+                mutators: Some(
+                    Mutators::from(
+                        [SuiteMutator::Setup(ParamSetup(vec![]))]
+                    )
+                ),
+                contents: Some(vec![
+                    parse_quote!{
+                        #[test_case]
+                        fn foo() {}
+                    }
+                ])
+            })
+        );
+    }
+
+    #[test]
+    fn parse_consumes_teardown_function() {
+        assert_eq_parsed!(
+            syn::parse2::<TestSuite>(quote!{
+                mod my_suite {
+                    #[teardown]
+                    fn teardown() {}
+
+                    #[test_case]
+                    fn foo() {}
+                }
+            }),
+            Ok(TestSuite {
+                name: parse_quote!(my_suite),
+                mutators: Some(
+                    Mutators::from(
+                        [SuiteMutator::Teardown(ParamTeardown(vec![]))]
+                    )
+                ),
+                contents: Some(vec![
+                    parse_quote!{
+                        #[test_case]
+                        fn foo() {}
+                    }
+                ])
+            })
+        );
+    }
+
+    #[test]
+    fn parse_works_with_no_setup_and_teardown() {
+        assert_eq_parsed!(
+            syn::parse2::<TestSuite>(quote!{
+                mod my_suite {
+                    #[test_case]
+                    fn foo() {}
+                }
+            }),
+            Ok(TestSuite {
+                name: parse_quote!(my_suite),
+                mutators: None,
+                contents: Some(vec![
+                    parse_quote!{
+                        #[test_case]
+                        fn foo() {}
+                    }
+                ])
+            })
+        );
+    }
+
+    #[test]
+    fn parse_works_with_empty_modules() {
+        assert_eq_parsed!(
+            syn::parse2::<TestSuite>(quote!{
+                mod my_suite {
+
+                }
+            }),
+            Ok(TestSuite {
+                name: parse_quote!(my_suite),
+                mutators: None,
+                contents: None
+            })
+        );
+    }
+
+    #[test]
+    fn mutate_is_ok_with_no_mutators() {
+        let suite = TestSuite {
+            name: parse_quote!(my_suite),
+            mutators: None,
+            contents: None
+        };
+
+        let mut items: [Item; 3] = [
+            parse_quote!(fn foo() {}),
+            parse_quote!(fn bar() {}),
+            parse_quote!(fn baz() {})
+        ];
+
+        items.iter_mut().for_each(| item |
+            assert_eq_mutate!(suite, item, Ok(()))
+        );
+    }
+
+    #[test]
+    fn mutate_applies_setup_and_teardown_in_order() {
+        let suite = TestSuite {
+            name: parse_quote!(my_suite),
+            mutators: Some(
+                // Defined the other way around
+                // purpose to test the Ord implementation
+                Mutators::from(
+                    [
+                        SuiteMutator::Teardown(ParamTeardown(vec![
+                            parse_quote!(let b = 456;)
+                        ])),
+                        SuiteMutator::Setup(ParamSetup(vec![
+                            parse_quote!(let a = 123;)
+                        ]))
+                    ]
+                )
+            ),
+            contents: None
+        };
+
+        let mut test = Item::Fn(parse_quote!(#[test] fn foo() {bar();}));
+        assert_eq_mutate!(suite, &mut test, Ok(()));
+        assert_eq_tokens!(
+            test, quote!{
+                #[test]
+                fn foo() {
+                    let a = 123;
+                    bar();
+                    let b = 456;
+                }
+            }
+        )
+    }
+
+    #[test]
+    fn mutate_only_affects_tests() {
+        let suite = TestSuite {
+            name: parse_quote!(my_suite),
+            mutators: Some(
+                Mutators::from(
+                    [
+                        SuiteMutator::Setup(ParamSetup(vec![
+                            parse_quote!(let a = 123;)
+                        ])),
+                        SuiteMutator::Teardown(ParamTeardown(vec![
+                            parse_quote!(let b = 456;)
+                        ]))
+                    ]
+                )
+            ),
+            contents: None
+        };
+
+        let mut items: [Item; 9] = [
+            parse_quote!(#[wasm_bindgen_test] fn one() {}),
+            parse_quote!(const SEED: usize = 123;),
+            parse_quote!(use crate::foo::*;),
+            parse_quote!(struct Foo;),
+            parse_quote!(#[test_case] fn two() {}),
+            parse_quote!(enum Bar{ZERO}),
+            parse_quote!(trait Baz{}),
+            parse_quote!(type Whiskey = Delta;),
+            parse_quote!(#[test] fn three() {}),
+        ];
+
+        items.iter_mut().for_each(| item |
+            assert_eq_mutate!(suite, item, Ok(()))
+        );
+
+        assert_eq_tokens!(
+            items[0], quote!{
+                #[wasm_bindgen_test]
+                fn one() {
+                    let a = 123;
+                    let b = 456;
+                }
+            }
+        );
+
+        assert_eq_tokens!(
+            items[4], quote!{
+                #[test_case]
+                fn two() {
+                    let a = 123;
+                    let b = 456;
+                }
+            }
+        );
+
+        assert_eq_tokens!(
+            items[8], quote!{
+                #[test]
+                fn three() {
+                    let a = 123;
+                    let b = 456;
+                }
+            }
+        );
+    }
+
+    #[test]
+    fn attribute_only_applies_to_modules() {
+        assert_eq_parsed!(
+            syn::parse2::<TestSuite>(quote!(fn invalid() {})),
+            Err(error_spanned!("#[test_suite] can only be applied to modules"))
+        );
+    }
+
+    #[test]
+    fn to_tokens_outputs_parsed_module_as_is() {
+        let suite: TestSuite = parse_quote!{
+            mod my_suite {
+                #[test_case]
+                fn my_test() {}
+            }
+        };
+
+        assert_eq_tokens!(
+            suite.to_token_stream(),
+            quote!{
+                mod my_suite {
+                    #[test_case]
+                    fn my_test() {}
+                }
+            }
+        )
+    }
+
+    #[test]
+    fn to_tokens_outputs_parsed_module_even_if_empty() {
+        let suite: TestSuite = parse_quote!(mod my_suite {});
+
+        assert_eq_tokens!(
+            suite.to_token_stream(),
+            quote!(mod my_suite {})
+        )
+    }
+
+    mod render_mod_name {
+        use super::*;
+        
+        use syn::parse_quote;
+
+        #[test]
+        fn outputs_mod_with_test_suite_name() {
+            let mut tokens = TokenStream::new();
+
+            render_mod_name(&TestSuite {
+                name: parse_quote!(my_suite),
+                mutators: None,
+                contents: None
+            }, &mut tokens);
+    
+            assert_eq_tokens!(tokens, quote!(mod my_suite));
+        }
+    }
+
+    mod is_test_attribute {
+        use super::*;
+        
+        use syn::AttrStyle;
+
+        #[test]
+        fn recognizes_test_case() {
+            assert!(is_test_attribute(
+                &[construct_attribute!(AttrStyle::Outer, test_case)]
+            ));
+        }
+
+        #[test]
+        fn recognizes_test() {
+            assert!(is_test_attribute(
+                &[construct_attribute!(AttrStyle::Outer, test)]
+            ));
+        }
+
+        #[test]
+        fn recognizes_wasm_bindgen_test() {
+            assert!(is_test_attribute(
+                &[construct_attribute!(AttrStyle::Outer, wasm_bindgen_test)]
+            ));
+        }
+
+        #[test]
+        fn does_not_recognize_other_attributes_named_test() {
+            assert!(!
+                is_test_attribute(
+                    &[
+                        construct_attribute!(AttrStyle::Outer, foo_test),
+                        construct_attribute!(AttrStyle::Outer, test_bar),
+                        construct_attribute!(AttrStyle::Outer, test_),
+                        construct_attribute!(AttrStyle::Outer, _test),
+                    ]
+                )
+            );
+        }
+    }
 }
