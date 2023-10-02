97c90b0062518b8988d7e88be6420af4af69f42e
nannafudge <6781691+nannafudge@users.noreply.github.com>
2023-10-02 17:02:34 +0100

    [#17] - Began creating docstrings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,68 @@
+//! # Sith
+//! > **S**imple **I**njectible **T**est **H**arness
+//! 
+//! Current Features:
+//! 
+//! ## `#[test_case]`
+//! 
+//! Functions may be affixed with the `#[test_case]` outer attribute
+//! to define a `sith` test. These tests may be parameterized: see 
+//! [`test_case`](core::test_case#parameters) for an up-to-date list of 
+//! available parameters and features.
+//! 
+//! ### Example
+//! 
+//! ```
+//! use sith::test_case;
+//! 
+//! #[test_case]
+//! fn unparameterized() {
+//!     println!("Hello from a unparameterized test!");
+//! }
+//! 
+//! #[test_case(with(123))]
+//! fn parameterized(my_arg: usize) {
+//!     println!("my_arg is: {}!", my_arg);
+//! }
+//! ```
+//! 
+//! ## `#[test_suite]`
+//! 
+//! Tests may be aggregated into modules or `suites`. Currently, modules
+//! affixed with the `#[test_suite]` attribute may define a common set of 
+//! `setup`/`teardown` routines: I.e, pre-test configuration and post-test cleanup.
+//! 
+//! ### Example
+//! 
+//! ```
+//! #[test_suite]
+//! mod suite {
+//!     use sith::test_case;
+//!     use std::io::Write;
+//!
+//!     #[setup]
+//!     fn setup() {
+//!         let handler = std::panic::take_hook();
+//!         std::panic::set_hook(Box::new(move | info | {
+//!            let _ = std::io::stderr().write_fmt(format_args!("failed with seed: {}\n", SEED));
+//!             handler(info);
+//!         })); 
+//!     }
+//! 
+//!     #[teardown]
+//!     fn teardown() {
+//!         // Re-register the default panic hook
+//!         let _ = std::panic::take_hook();
+//!     }
+//! 
+//!     #[test_case]
+//!     fn unparameterized() {
+//!         println!("Hello from a unparameterized test!");
+//!     }
+//! }
+//! 
+//! ```
+
 use proc_macro::TokenStream;
 
 mod common;
